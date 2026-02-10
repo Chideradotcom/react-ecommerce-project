e35d10d1a413ab57e6c6c37a5a50c7905f58997d
Chidera Chikwendu <wisdomchikwendu2005@gmail.com>
2026-02-10 12:59:06 +0100

    Created the tracking page and moved the images to the assets folder and import them to the appropriate components

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import { NavLink } from 'react-router';
 import './header.css'
+import CartIconImage from "../assets/images/icons/cart-icon.png"
+import SearchIconImage from "../assets/images/icons/search-icon.png"
+import LogoWhiteImage from "../assets/images/logo-white.png"
+import MobileLogoWhiteImage from "../assets/images/mobile-logo-white.png"
 
 export function Header() {
   return (
     <div className="header">
       <div className="left-section">
         <NavLink to="/" className="header-link">
-          <img className="logo" src="images/logo-white.png" />
-          <img className="mobile-logo" src="images/mobile-logo-white.png" />
+          <img className="logo" src={LogoWhiteImage} />
+          <img className="mobile-logo" src={MobileLogoWhiteImage} />
         </NavLink>
       </div>
 
@@ -15,7 +19,7 @@ export function Header() {
         <input className="search-bar" type="text" placeholder="Search" />
 
         <button className="search-button">
-          <img className="search-icon" src="images/icons/search-icon.png" />
+          <img className="search-icon" src={SearchIconImage} />
         </button>
       </div>
 
@@ -25,7 +29,7 @@ export function Header() {
         </NavLink>
 
         <NavLink className={({ isActive }) => `cart-link header-link ${isActive ? 'active' : ''}`} to="/checkout">
-          <img className="cart-icon" src="images/icons/cart-icon.png" />
+          <img className="cart-icon" src={CartIconImage} />
           <div className="cart-quantity">3</div>
           <div className="cart-text">Cart</div>
         </NavLink>
--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,5 +1,6 @@
 import { Header } from "../components/Header";
 import "./OrdersPage.css";
+import BuyAgainImage from "../assets/images/icons/buy-again.png"
 
 export function OrdersPage() {
   return (
@@ -48,7 +49,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainImage}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
@@ -77,7 +78,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainImage}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
@@ -126,7 +127,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainImage}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>

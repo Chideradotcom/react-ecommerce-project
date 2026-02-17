cbf17c36f7764d749e9d2d174976e2a33cecc4a1
Chidera Chikwendu <wisdomchikwendu2005@gmail.com>
2026-02-17 13:00:03 +0100

    Add server proxy config for '/api' and '/images'

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      },
+      '/images': {
+        target: 'http://localhost:3000'
+      }
+    }
   //   host: '0.0.0.0', // bind all interfaces (try if localhost binding is blocked)
   //   port: 30000,
-  // },
+  },
 })
